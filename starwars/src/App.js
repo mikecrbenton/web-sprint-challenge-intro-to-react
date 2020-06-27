ead25ea17cf454c2def33f9259ed6c9ba28fa0f3
mikecrbenton <mikecrbenton@hotmail.com>
2020-06-27 11:32:57 -0500

    Downloading dependencies - able to retrieve API Data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+function App() {
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+   const [characters, setCharacters] = useState();
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+
+   useEffect( () => {
+      axios.get('https://swapi.dev/api/people/')
+         .then( (response) => {      
+               setCharacters(response.data.results)       
+         })
+         .catch( e => console.log("ERROR = ", e));
+   }, []);
+
+   console.log('CHARACTERS IN APP' ,characters);
+
+   return (
+      <div>
+
+      </div>
+   );
+
+
 }
 
 export default App;
